e031f1e0598e6ef3036e8a261c58e69adfa831f9
mansi-mishra <mishramansi0407@gmail.com>
2022-03-10 19:46:51 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Features from "../components/Features/Features"
+import Hero from "../components/Hero/Hero"
+import Layout from "../components/Layout/Layout"
+import Team from "../components/Team/Team"
+
+const Index = () => {
+  return (
+    <Layout>
+      <Hero/>
+      <Features/>
+      <Team/>
+    </Layout>
+  )
+}
+
+export default Index
